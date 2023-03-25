90e4f40b579c6a084907bb33f61d1e3ef67d901b
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-03-25 07:41:04 +0300

    Added validations

--- app/models/power.rb
+++ app/models/power.rb
@@ -1,4 +1,7 @@
 class Power < ApplicationRecord
     has_many :hero_powers
     has_many :heroes, through: :hero_powers
+
+    # validations
+    validates :description, presence: true, length: { minimum:20 }
 end
